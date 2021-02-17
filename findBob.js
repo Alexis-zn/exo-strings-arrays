2b1a074451b58888bffd5911cb59f259a819674d
Alexis-zn <alexis.zentici@gmail.com>
2021-02-17 16:39:11 +0100

    Exo done

---
+++ findBob.js
@@ -0,0 +1,13 @@
+let str =
+  '\n\n\t\t    AlIcE eT bOb EsSaYeNt De CoMmUnIqUeR sEcReTeMeNt, MaIs EvE vEiLlE  \n\n '.toLowerCase()
+
+let clean = str.trim()
+    console.log(clean)
+
+let name1 = 'bob'
+
+if(str.includes(name1)) {
+  console.log(`This text include ${name1} `)
+} else {
+  console.log(`This text does not include ${name1} `)
+}
