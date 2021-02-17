b2326eeae71e4e7fee3f14fabf004f9d81506084
Alexis-zn <alexis.zentici@gmail.com>
2021-02-17 15:56:55 +0100

    Exo done

---
+++ check.js
@@ -0,0 +1,10 @@
+let text = 'JS, ECMAScript, esm6 peuvent être considérés comme des alias de Javascript'
+let word = 'Javascript'
+
+if(text.includes(word)) {
+  console.log(`This text includes the word ${word}`)
+} else {
+  console.log(`This text does not include the word ${word} `)
+}
+
+
