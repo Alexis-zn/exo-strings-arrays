f397e1d3e53a17f620af316912e42fd6815b5495
Alexis-zn <alexis.zentici@gmail.com>
2021-02-17 16:26:16 +0100

    Exo done

---
+++ countWords.js
@@ -0,0 +1,25 @@
+const text = `Je suis le ténébreux, - le veuf, - l'inconsolé,
+Le prince d'Aquitaine à la tour abolie :
+Ma seule étoile est morte, - et mon luth constellé
+Porte le soleil noir de la Mélancolie.
+
+Dans la nuit du tombeau, toi qui m'as consolé,
+Rends-moi le Pausilippe et la mer d'Italie,
+La fleur qui plaisait tant à mon cœur désolé,
+Et la treille où le pampre à la rose s'allie.
+
+Suis-je Amour ou Phébus ? ... Lusignan ou Biron ?
+Mon front est rouge encor du baiser de la reine ;
+J'ai rêvé dans la grotte où nage la sirène...
+
+Et j'ai deux fois vainqueur traversé l'Achéron ;
+Modulant tour à tour sur la lyre d'Orphée
+Les soupirs de la sainte et les cris de la fée.`
+
+const textSplit = text.split(` `)
+let nb = 0
+
+for(i=0; i < textSplit.length; i++) 
+if(textSplit[i])
+  nb++ 
+  console.log(`There is ${nb} characters in this text`)
