ff77ad231b3d0046a353e07c483b90eea966ccd6
Alexis-zn <alexis.zentici@gmail.com>
2021-02-18 10:32:44 +0100

    update

--- countWords.js
+++ countWords.js
@@ -1,25 +1,18 @@
-const text = `Je suis le ténébreux, - le veuf, - l'inconsolé,
+let text = `Je suis le ténébreux, - le veuf, - l'inconsolé,
 Le prince d'Aquitaine à la tour abolie :
 Ma seule étoile est morte, - et mon luth constellé
 Porte le soleil noir de la Mélancolie.
-
 Dans la nuit du tombeau, toi qui m'as consolé,
-Rends-moi le Pausilippe et la mer d'Italie,
+Rends - moi le Pausilippe et la mer d'Italie,
 La fleur qui plaisait tant à mon cœur désolé,
 Et la treille où le pampre à la rose s'allie.
-
-Suis-je Amour ou Phébus ? ... Lusignan ou Biron ?
-Mon front est rouge encor du baiser de la reine ;
+Suis - je Amour ou Phébus ? ...Lusignan ou Biron ?
+Mon front est rouge encor du baiser de la reine;
 J'ai rêvé dans la grotte où nage la sirène...
-
-Et j'ai deux fois vainqueur traversé l'Achéron ;
+Et j'ai deux fois vainqueur traversé l'Achéron;
 Modulant tour à tour sur la lyre d'Orphée
 Les soupirs de la sainte et les cris de la fée.`
 
-const textSplit = text.split(` `)
-let nb = 0
-
-for(i=0; i < textSplit.length; i++) 
-if(textSplit[i])
-  nb++ 
-  console.log(`There is ${nb} characters in this text`)
+text = text.replaceAll('\n', ' ')
+let tab = text.split(' ')
+console.log(`Le nombre de mot est ${tab.length}.`)
